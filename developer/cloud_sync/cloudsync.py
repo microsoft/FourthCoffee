04f7ca78a89df4b70903b9f81149c4d0e9923e49
Braulio Chavez <brauchsa@hotmail.com>
2023-04-25 21:51:01 -0700

    Added Cloud Sync feature

---
+++ developer/cloud_sync/cloudsync.py
@@ -0,0 +1,50 @@
+import mysql.connector
+from azure.cosmos import CosmosClient
+import time
+import os
+
+# MySQL connection settings
+server = os.environ.get('DBHOST')
+database = os.environ.get('DBNAME')
+username = os.environ.get('DBUSER')
+password = os.environ.get('DBSECRET')
+sync_interval = 120 #In seconds
+
+# Azure Cosmos DB connection settings
+endpoint = ''
+key = ''
+database_name = ''
+container_name = ''
+
+# Connect to MySQL
+cnxn = mysql.connector.connect(user=username, password=password, host=server, database=database)
+cursor = cnxn.cursor()
+
+# Connect to Azure Cosmos DB
+client = CosmosClient(endpoint, key)
+database = client.get_database_client(database_name)
+container = database.get_container_client(container_name)
+
+while True:
+    # Get all records with cloudSynced = 0
+    query = "SELECT * FROM Orders WHERE cloudSynced = 0"
+    cursor.execute(query)
+    rows = cursor.fetchall()
+
+    # Sync each record to Azure Cosmos DB and set cloudSynced to True
+    for row in rows:
+        document = {
+            'id': str(row[0]),
+            'orderDate': str(row[1]),
+            'orderdetails': row[2],
+            'storeId': str(row[3]),
+            'cloudSynced': True
+        }
+        container.upsert_item(document)
+        query = "UPDATE Orders SET cloudSynced = 1 WHERE orderID = " + str(row[0])
+        print("Order ID:",row[0],"synced to cloud")
+        cursor.execute(query)
+        cnxn.commit()
+
+    # Wait for 2 minutes before syncing again
+    time.sleep(sync_interval)
